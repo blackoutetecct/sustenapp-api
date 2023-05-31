b6c1da84c12625fb9a417ee5bf4fa0f9a30c374e
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-05-31 15:11:15 -0300

    Delete sustenapp_api directory

--- sustenapp_api/src/main/java/sustenapp_api/component/client/TarifaClientObject.java
+++
@@ -1,8 +0,0 @@
-package sustenapp_api.component.client;
-
-public class TarifaClientObject {
-    private String
-            agente, validadesde, subgrupo, modalidade,
-            acessante, classe, subclasse, posto;
-    private double tarifademandatusd, tarifaconsumotusd, tarifaconsumote;
-}
