39c06a544249c0e9055c1615485e1f075ede8783
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-08-23 14:56:29 -0300

    Delete sustenApp/src/main directory

--- sustenApp/src/main/java/sustenapp_api/component/treatment/StringTreatment.java
+++
@@ -1,7 +0,0 @@
-package sustenapp_api.component.treatment;
-
-public class StringTreatment {
-    public static String removeEspeciais(String texto) {
-        return texto.replaceAll("/[^a-z0-9]/gi", "");
-    }
-}
